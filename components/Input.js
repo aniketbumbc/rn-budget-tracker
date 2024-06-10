089deaf79bedbbb38d0b5774bf54a4da4529aa7a
aniket <aniket.umbc@gmail.com>
2024-06-10 22:15:59 +0530

    styling input

--- components/Input.js
+++ components/Input.js
@@ -1,12 +1,42 @@
-import { Text, TextInput, View } from 'react-native';
+import { Text, TextInput, View, StyleSheet } from 'react-native';
+import { GlobalStyles } from '../constant/styles';
 
 function Input({ label, textInputConfig }) {
+  const inputStyle = [styles.input];
+
+  if (textInputConfig && textInputConfig.multiline) {
+    inputStyle.push(styles.inputMultiline);
+  }
+
   return (
-    <View>
-      <Text>{label}</Text>
-      <TextInput {...textInputConfig} />
+    <View style={styles.inputContainer}>
+      <Text style={styles.labelText}>{label}</Text>
+      <TextInput {...textInputConfig} style={inputStyle} />
     </View>
   );
 }
 
 export default Input;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    marginHorizontal: 4,
+    marginVertical: 8,
+  },
+  labelText: {
+    fontSize: 16,
+    color: GlobalStyles.colors.primary100,
+    marginBottom: 4,
+  },
+  input: {
+    backgroundColor: GlobalStyles.colors.primary100,
+    padding: 6,
+    borderRadius: 6,
+    fontSize: 20,
+    color: GlobalStyles.colors.primary700,
+  },
+  inputMultiline: {
+    minHeight: 150,
+    textAlignVertical: 'top',
+  },
+});
